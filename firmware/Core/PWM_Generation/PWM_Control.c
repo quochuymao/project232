b171d1c45ae4632198b2d7616461eb4fefc73599
tuanthanhhh <letuanthanh.work@gmail.com>
2024-05-11 19:42:19 +0700

    update PWM fucntion

--- firmware/Core/PWM_Generation/PWM_Control.c
+++ firmware/Core/PWM_Generation/PWM_Control.c
@@ -4,11 +4,8 @@ const int TIM_CLOCK = 24000000;
 
 void PWM_Control(int dutyCycle, int frequency)
 {
-	int tim = TIM1->PSC;
 	TIM1->ARR  =  TIM_CLOCK/frequency;
-	int arr = TIM1->ARR;
 	TIM1->CCR1 = ((dutyCycle)*(TIM1->ARR))/100;
-	int ccr = TIM1->CCR1;
 }
 
 void PWM_Enable(void)
